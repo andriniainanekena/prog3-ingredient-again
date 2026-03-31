7a5948786bc25fb5f6905c65446d10a4d2b987d7
Nekena Andriniaina <andriniainanekena@gmail.com>
2026-03-31 09:24:35 +0300

    Update password and username -> DataSourceConfig

--- src/main/java/ingredientmanagement/datasource/DataSourceConfig.java
+++ src/main/java/ingredientmanagement/datasource/DataSourceConfig.java
@@ -11,11 +11,16 @@ public class DataSourceConfig {
 
     @Bean
     public DataSource dataSource() {
+        String url = System.getenv().getOrDefault("JDBC_URL", "jdbc:postgresql://localhost:5432/ingredientdb");
+        String username = System.getenv().getOrDefault("USERNAME", "ingredientuser");
+        String password = System.getenv().getOrDefault("PASSWORD", "ingredientpass");
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(System.getenv("JDBC_URL"));
-        dataSource.setUsername(System.getenv("USERNAME"));
-        dataSource.setPassword(System.getenv("PASSWORD"));
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
         return dataSource;
     }
 }
