a082602f609afe9130cc29b0204f862a99f3ab4a
Nekena Andriniaina <andriniainanekena@gmail.com>
2026-03-31 07:40:12 +0300

    Add IngredientSpringBootApplication class

---
+++ src/main/java/ingredientmanagement/IngredientSpringBootApplication.java
@@ -0,0 +1,14 @@
+package ingredientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientSpringBootApplication.class, args);
+    }
+
+}
+
