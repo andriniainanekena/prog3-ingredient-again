e43d621b93872f7fce63f28ebc22e8de0704cdff
Nekena Andriniaina <andriniainanekena@gmail.com>
2026-03-31 08:00:13 +0300

    Add StockValue entity class with quantity and unit

---
+++ src/main/java/ingredientmanagement/entity/StockValue.java
@@ -0,0 +1,15 @@
+package ingredientmanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockValue {
+    private Double quantity;
+    private UnitEnum unit;
+}
